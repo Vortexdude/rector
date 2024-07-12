bbe328cf401d87d6c3e48928fc163258d234b927
vortexdude <nitinnamdeo456@gmail.com>
2024-07-12 16:58:29 +0530

    :hammer: Enhacement for ssl util

--- app/api/routes/ssl_cert_util.py
+++ app/api/routes/ssl_cert_util.py
@@ -8,12 +8,27 @@ ssl_context = ssl.create_default_context()
 
 @router.get("/ssl_cert")
 def get_ssl_cert_details(domain: str):
-    return get_ssl_details(domain)
+    url = validate_url(domain)
+    try:
+        detail = get_ssl_details(url)
+    except ValueError:
+        detail = {"status": "Invalid URI"}
+
+    return detail
 
 
-def get_ssl_details(domain: str):
-    with ssl_context.wrap_socket(socket.socket(), server_hostname=domain) as s:
-        s.connect((domain, 443))
+def validate_url(url: str) -> str:
+    if url.startswith("https"):
+        url = url.lstrip("https://")
+
+    if "www" in url:
+        url = url.lstrip("www.")
+    return url
+
+
+def get_ssl_details(url: str):
+    with ssl_context.wrap_socket(socket.socket(), server_hostname=url) as s:
+        s.connect((url, 443))
         cert = s.getpeercert()
         issuer = cert['issuer'][1][0][1]
         date_format = "%b %d %H:%M:%S %Y %Z"
@@ -24,7 +39,7 @@ def get_ssl_details(domain: str):
             alt_name = cert['subjectAltName'][1][1]
 
     return {
-            'domain': domain,
+            'domain': url,
             'altname': alt_name,
             'issuer': issuer,
             'issue_date': issuer_date.strftime("%d-%m-%Y"),
