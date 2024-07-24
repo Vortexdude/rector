37540966d1d42bd73cee68f586a9ba218c7fb395
vortexdude <nitinnamdeo456@gmail.com>
2024-07-24 23:58:10 +0530

    :v: remove logging from routes

--- app/api/routes/auth/__init__.py
+++ app/api/routes/auth/__init__.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Body
-from app.core.config import logger
 from app.core.db import db_dependency
 from app.api.schema import UserCreateSchema, UserLoginSchema, UserBase
 from app.api.services import UserService
@@ -12,17 +11,14 @@ router = APIRouter()
 
 @router.get("/me")
 def read_own_items(db: db_dependency, user: user_identity_dependency) -> UserBase:
-    logger.debug(f"GET /me > with {user.email}")
     return UserService(db).current_user(user.email)
 
 
 @router.post("/signup")
 def user_register(db: db_dependency, data: UserCreateSchema = Body()):
-    logger.debug(f"POST /signup > with {data}")
     return UserService(db).register_user(data)
 
 
 @router.post("/login")
 async def login_for_access_token(db: db_dependency, form_data: UserLoginSchema) -> Token:
-    logger.debug(f"POST /login > with {form_data.email}")
     return UserService(db).login(form_data)
