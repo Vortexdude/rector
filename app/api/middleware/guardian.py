9aed82b9d244569283ef9d151466e92d7f0ab76d
vortexdude <nitinnamdeo456@gmail.com>
2024-07-25 19:28:24 +0530

    :envelope: add logger for the file

--- app/api/middleware/guardian.py
+++ app/api/middleware/guardian.py
@@ -1,3 +1,4 @@
+from app.common.utils.timezone import timezone
 from app.core.config import logger
 from fastapi import FastAPI
 from starlette.requests import Request
@@ -13,5 +14,11 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         super().__init__(app)
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-
-        return await call_next(request)
+        start_time = timezone.now()
+        response = await call_next(request)
+        end_time = timezone.now()
+        total_time = (end_time - start_time).total_seconds() * 1000.0
+        logger.info(
+            f"logger=rector,{request.client.host=},{request.method=},{response.status_code=},{total_time:.5f}ms"
+        )
+        return response
