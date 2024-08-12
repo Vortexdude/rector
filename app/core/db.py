1b532d89a46eb23c7cc400511e2cd947935d8318
vortexdude <nitinnamdeo456@gmail.com>
2024-08-12 18:57:13 +0530

    :v: chalta hua code

--- app/core/db.py
+++ app/core/db.py
@@ -38,6 +38,7 @@ def display_metadata(meta_data: metadata = metadata):
         PRIMARY_KEY = "---Primary Key---"
         NULLABLE = "---Nullable---"
         DEFAULT = "--Default--"
+        FOREIGN_KEY = "---Foreign Key---"
 
         def __init__(self):
             self._col = self.COLUMN, len(self.COLUMN)
@@ -45,28 +46,34 @@ def display_metadata(meta_data: metadata = metadata):
             self._pk = self.PRIMARY_KEY, len(self.PRIMARY_KEY)
             self._nul = self.NULLABLE, len(self.NULLABLE)
             self._def = self.DEFAULT, len(self.DEFAULT)
+            self._fk = self.FOREIGN_KEY, len(self.FOREIGN_KEY)
 
         @property
         def heading(self) -> str:
-            return f"+{self._col[0]}+{self._type[0]}+{self._pk[0]}+{self._nul[0]}+{self._def[0]}+"
+            return f"+{self._col[0]}+{self._type[0]}+{self._pk[0]}+{self._nul[0]}+{self._def[0]}+{self._fk[0]}+"
 
         @property
         def footer(self) -> str:
-            return f"+{'-'*self._col[1]}+{'-'*self._type[1]}+{'-'*self._pk[1]}+{'-'*self._nul[1]}+{'-'*self._def[1]}+"
+            return f"+{'-'*self._col[1]}+{'-'*self._type[1]}+{'-'*self._pk[1]}+{'-'*self._nul[1]}+{'-'*self._def[1]}+{'-'*self._fk[1]}+"
 
-    _col = "-----Column-----"
-    _type = "-----Type-----"
-    _pk = '---Primary Key---'
-    _nul = "---Nullable---"
-    _def = "--Default--"
+        def print_column(self, column):
+            _name = str(column.name)
+            _type = str(column.type)
+            _pk = str(column.primary_key)
+            _nul = str(column.nullable)
+            _def = str(column.default)
+            _fk = ", ".join([column.table.name + "." + fk.column.name for fk in column.foreign_keys]) or "None"
+            return f"|{_name:^{self._col[1]}}|{_type:^{self._type[1]}}|{_pk:^{self._pk[1]}}|{_nul:^{self._nul[1]}}|{_def:^{self._def[1]}}|{_fk:^{self._fk[1]}}|"
+
+    ds = DisplayCols()
+
     logger.debug("Fetching Database Tables . . ")
     for table_name, table in meta_data.tables.items():
         logger.debug(f"Table : {str(table_name):=^20}")
-        logger.debug(f"+{_col}+{_type}+{_pk}+{_nul}+{_def}+")
+        logger.debug(ds.heading)
         for column in table.columns:
-            logger.debug(f"|{str(column.name):^{len(_col)}}|{str(column.type):^14}|{str(column.primary_key): ^17}"
-                         f"|{str(column.nullable): ^14}|{str(column.default): ^11}|")
-        logger.debug(f"+{'-'*len(_col)}+{'-'*len(_type)}+{'-'*len(_pk)}+{'-'*len(_nul)}+{'-'*len(_def)}+")
+            logger.debug(ds.print_column(column))
+        logger.debug(ds.footer)
 
 
 class ModelMixing(object):
